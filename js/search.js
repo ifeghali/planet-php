2c7a29838ea53f54fea571984135b418ecb16c16
Igor Feghali <ifeghali@php.net>
2011-08-21 13:43:41 -0300

    cosmetic fixes

--- js/search.js
+++ js/search.js
@@ -1,22 +1,22 @@
-  function niceURL () {
-            var value=document.getElementById("searchtext").value;
-            window.location.href = "/search/" + value;
-            return false;
-          }
+function niceURL () {
+    var value=document.getElementById("searchtext").value;
+    window.location.href = "/search/" + value;
+    return false;
+}
 
 function addEngine()
+{
+    if ((typeof window.sidebar == "object") && (typeof window.sidebar.addSearchEngine == "function"))
     {
-        if ((typeof window.sidebar == "object") && (typeof window.sidebar.addSearchEngine == "function"))
-        {
-            window.sidebar.addSearchEngine(
+        window.sidebar.addSearchEngine(
                 "planet-php.src",            /* engine URL */
                 "themes/img/planet-phprio.jpg", /* icon URL */
                 "Planet PHP Rio",               /* engine name */
                 "Web" );                        /* category name */
-        }
-        else
-        {
-            alert("Mozilla M15 or later is required to add a search engine.");
-        }
     }
+    else
+    {
+        alert("Mozilla M15 or later is required to add a search engine.");
+    }
+}
 
