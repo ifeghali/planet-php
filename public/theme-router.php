86ef09be2e17c5faa37f89cef72b4c4ea8ebbddc
Igor Feghali <ifeghali@php.net>
2011-08-22 16:32:01 -0300

    fixed a bug in theme router

--- public/theme-router.php
+++ public/theme-router.php
@@ -4,9 +4,15 @@ if (!include dirname(__FILE__) . '/../inc/config.inc.php') {
     die("No conf.");
 }
 
-$query = (string) @$_GET['resource'];
+$query = (string) @$_GET['r'];
 if (empty($query)) {
     $query = null;
 }
 
-header('location: ../themes/'.$BX_config['theme'].'/'.$query);
+header('location: '.sprintf(
+    '%s/%s/%s/%s',
+    dirname($_SERVER['SCRIPT_NAME']),
+    'themes',
+    $BX_config['theme'],
+    $query
+));
