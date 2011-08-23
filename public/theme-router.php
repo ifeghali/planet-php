fb52fee7ceaac9dea9badacdf38317fe4b047ce0
Igor Feghali <ifeghali@php.net>
2011-08-23 17:11:32 -0300

    fixed theme-router again
    last commit to theme-router fixed the case for planets hosted on root
    level, but broke for the ones hosted on a subdir. fortunately, this
    commit should fix both cases. basically, we cannot redirect to
    //some-path/resource, that should be /some-path/resource instead. the
    problem is caused when the the script path is '/' so dirname() returns
    the root dir '/'. For all the other cases it returns
    'somedir/somesubdir' without the leading slash.

--- public/theme-router.php
+++ public/theme-router.php
@@ -9,10 +9,29 @@ if (empty($query)) {
     $query = null;
 }
 
+/**
+ * Ok, we get incosistent results from dirname() depending when the script is 
+ * hosted at root level or inside a subdir, so we are going to add the slashes 
+ * manually
+ */
+$path = array_filter(explode('/', dirname($_SERVER['SCRIPT_NAME'])));
+
+/**
+ * themes folder
+ */
+$path[] = 'themes';
+
+/**
+ * chosen theme
+ */
+$path[] = $BX_config['theme'];
+
+/**
+ * requested file
+ */
+$path[] = $query;
+
 header('location: '.sprintf(
-    '%s%s/%s/%s',
-    dirname($_SERVER['SCRIPT_NAME']),
-    'themes',
-    $BX_config['theme'],
-    $query
+    '/%s',
+    implode('/', $path)
 ));
