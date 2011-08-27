1f11aaa2deeadc20cbd991e4ada6c004d221a5f4
Igor Feghali <ifeghali@php.net>
2011-08-26 21:41:13 -0300

    admin is now using MDB2

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,17 +1,18 @@
 <?php
-error_reporting(error_reporting() & ~E_STRICT);
-require_once dirname(__FILE__) . '/../inc/config-admin.php';
+require_once '../../inc/config-admin.php';
+require_once '../../inc/config.inc.php';
 require_once 'Auth.php';
 require_once 'Log.php';
 require_once 'Log/observer.php';
 require_once 'Validate.php';
 require_once 'HTML/Template/IT.php';
+require_once 'MDB2.php';
 
 class Planet
 {
 	protected $_db;
 	
-	public function __construct(PDO $db) 
+	public function __construct($db = null) 
 	{
 		$this->_db = $db;
 	}
@@ -47,9 +48,8 @@ class Planet
 			throw new Exception('Invalid RSS');
 		}
 		
-		$stmt = $this->_db->prepare('INSERT INTO feeds SET link = :url');
-		$stmt->bindParam(':url', $url, PDO::PARAM_STR);
-		return $stmt->execute();
+		$stmt = $this->_db->prepare('INSERT INTO feeds SET link = ?', array('text'));
+		return $stmt->execute($url);
 	}
 
 	public function getFeeds() 
@@ -57,7 +57,7 @@ class Planet
 		$results = array();
 		$stmt    = $this->_db->query("SELECT id, link FROM feeds ORDER BY ID");
 
-		while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+		while ($row = $stmt->fetchRow(MDB2_FETCHMODE_ASSOC))
 		{
 			$results[] = $row;
 		}
@@ -71,9 +71,8 @@ class Planet
 			throw new Exception('Cannot delete an empty id');
 		}
 		
-		$stmt = $this->_db->prepare('DELETE FROM feeds WHERE ID = :id');
-		$stmt->bindParam(':id', $id, PDO::PARAM_INT);
-		return $stmt->execute();
+		$stmt = $this->_db->prepare('DELETE FROM feeds WHERE ID = ?', array('integer'));
+		return $stmt->execute($id);
 	}
 }
 
@@ -96,25 +95,21 @@ function loginFunction($username = null, $status = null, &$auth = null)
     echo '</form>';
 }
 
-/*$options = array(
-//    'url'   => 'https://pear.php.net/rest-login.php',
-//    'karma' => 'pear.planet.admin',
-	'karma' => 'pear.dev'
-	); */
-$options = array();
-$auth = new Auth("PEAR", $options, 'loginFunction');
+$auth = new Auth(
+    $ADMIN_config['auth_container'],
+    $ADMIN_config['auth'],
+    'loginFunction'
+);
 $auth->start();
+
+$db = MDB2::connect($BX_config['dsn']);
+$planet = new Planet($db);
 
 $debugObserver = new Log_observer(PEAR_LOG_DEBUG);
 $auth->attachLogObserver($debugObvserver);
 
-$it = new HTML_Template_IT('./templates');
-$it->loadTemplatefile('index.tpl', true, true);
-
-$pdo = new PDO(PDO_DSN, PDO_USERNAME, PDO_PASSWORD);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$planet = new Planet($pdo);
+$it = new HTML_Template_IT(TEMPLATE_DIR.$BX_config['theme']);
+$it->loadTemplatefile('admin.tpl', true, true);
 
 if (isset($_GET['logout']) && $auth->checkAuth()) {
     $auth->logout();
