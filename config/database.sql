de93cdf72f940fd653aadf606dad9a9c34a82969
Igor Feghali <ifeghali@php.net>
2011-09-07 15:55:51 -0300

    update database dump (was missing auth table)

--- config/database.sql
+++ config/database.sql
@@ -1,121 +1,144 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.5.12, for osx10.6 (i386)
 --
--- Host: localhost    Database: planet-php
+-- Host: localhost    Database: planet
 -- ------------------------------------------------------
--- Server version	5.0.51a-24+lenny1
+-- Server version	5.5.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,MYSQL40' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `auth`
+--
+
+DROP TABLE IF EXISTS `auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth` (
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(32) NOT NULL DEFAULT '',
+  PRIMARY KEY (`username`),
+  KEY `password` (`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `blogs`
 --
 
 DROP TABLE IF EXISTS `blogs`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `blogs` (
-  `ID` int(11) NOT NULL auto_increment,
-  `link` varchar(255) NOT NULL default '',
-  `title` varchar(255) NOT NULL default '',
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `link` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
   `description` tinytext,
-  `changed` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `author_old` varchar(100) NOT NULL default '',
-  `dontshowblogtitle` tinyint(4) NOT NULL default '1',
-  PRIMARY KEY  (`ID`),
+  `changed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `author_old` varchar(100) NOT NULL DEFAULT '',
+  `dontshowblogtitle` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`ID`),
   UNIQUE KEY `link` (`link`)
-) ENGINE=MyISAM; 
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `entries`
 --
 
 DROP TABLE IF EXISTS `entries`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `entries` (
-  `ID` int(11) NOT NULL default '0',
-  `feedsID` int(11) NOT NULL default '0',
+  `ID` int(11) NOT NULL DEFAULT '0',
+  `feedsID` int(11) NOT NULL DEFAULT '0',
   `title` tinytext,
   `link` tinytext NOT NULL,
   `description` text,
-  `dc_date` datetime default '0000-00-00 00:00:00',
-  `dc_creator` varchar(100) default NULL,
+  `dc_date` datetime DEFAULT '0000-00-00 00:00:00',
+  `dc_creator` varchar(100) DEFAULT NULL,
   `content_encoded` text,
-  `changed` timestamp NOT NULL,
-  `md5` varchar(32) NOT NULL default '',
-  `guid` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`ID`),
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `md5` varchar(32) NOT NULL DEFAULT '',
+  `guid` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`),
   UNIQUE KEY `guid` (`guid`(250)),
   KEY `rss_feed_ID` (`feedsID`),
   FULLTEXT KEY `search` (`description`,`content_encoded`,`title`)
-) ENGINE=MyISAM;
-SET character_set_client = @saved_cs_client;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `feeds`
 --
 
 DROP TABLE IF EXISTS `feeds`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `feeds` (
-  `ID` int(11) NOT NULL auto_increment,
-  `blogsID` int(11) NOT NULL default '0',
-  `link` varchar(255) NOT NULL default '',
-  `changed` timestamp NOT NULL,
-  `cats` varchar(255) NOT NULL default '',
-  `section` varchar(50) NOT NULL default 'default',
-  `author` varchar(200) NOT NULL default '',
-  PRIMARY KEY  (`ID`),
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `blogsID` int(11) NOT NULL DEFAULT '0',
+  `link` varchar(255) NOT NULL DEFAULT '',
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `cats` varchar(255) NOT NULL DEFAULT '',
+  `section` varchar(50) NOT NULL DEFAULT 'default',
+  `author` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`),
   UNIQUE KEY `rssURL` (`link`),
   KEY `blogID` (`blogsID`),
   KEY `section` (`section`)
-) ENGINE=MyISAM; 
-SET character_set_client = @saved_cs_client;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `planet_seq`
 --
 
 DROP TABLE IF EXISTS `planet_seq`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `planet_seq` (
-  `sequence` int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (`sequence`)
-) ENGINE=MyISAM;
-SET character_set_client = @saved_cs_client;
+  `sequence` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`sequence`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `submissions`
 --
 
 DROP TABLE IF EXISTS `submissions`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `submissions` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(200) NOT NULL default '',
-  `url` varchar(200) NOT NULL default '',
-  `rss` varchar(200) NOT NULL default '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL DEFAULT '',
+  `url` varchar(200) NOT NULL DEFAULT '',
+  `rss` varchar(200) NOT NULL DEFAULT '',
   `description` text NOT NULL,
-  `changed` timestamp NOT NULL,
-  `email` varchar(200) NOT NULL default '',
-  `state` tinyint(4) NOT NULL default '0',
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(200) NOT NULL DEFAULT '',
+  `state` tinyint(4) NOT NULL DEFAULT '0',
   `rejectreason` text NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM; 
-SET character_set_client = @saved_cs_client;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-04-30 16:02:31
+-- Dump completed on 2011-09-07 15:54:30
